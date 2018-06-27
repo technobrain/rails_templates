ae32583ec41e95d0098ad493bf6360181521ec34
KOBAYASHI-Toshinobu <toshinobu_kobayashi@tbn.co.jp>
2018-06-28 00:27:36 +0900

    fix gem_group

--- 5.2/template_basic_app.rb
+++ 5.2/template_basic_app.rb
@@ -17,7 +17,7 @@ gem 'listen', '>= 3.0.5', '< 3.2'
 # Statemachine
 gem 'aasm'
 
-group :development, :test do
+gem_group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger
   # console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
@@ -27,7 +27,7 @@ group :development, :test do
   gem 'rubocop', require: false
 end
 
-group :development do
+gem_group :development do
   # Spring speeds up development by keeping your application running
   # in the background. Read more: https://github.com/rails/spring
   gem 'spring'
