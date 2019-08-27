93185565aaa3bbb3b504dcfc0ed04e06a729a630
小林理伸 <toshinobu_kobayashi@tbn.co.jp>
2019-08-27 09:14:00 +0900

    Add initializer

--- 6.0/template_ns.rb
+++ 6.0/template_ns.rb
@@ -66,6 +66,54 @@ end
 run 'bundle install'
 rake 'db:create'
 
+environment "config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]"
+environment "config.i18n.default_locale = :ja"
+environment "config.time_zone = 'Tokyo'"
+
+initializer 'ukstyle.rb', <<-CODE
+  Rails.application.config.generators do |g|
+    g.template_engine :slim
+    g.orm :active_record
+    g.assets false
+    g.helper false
+    g.scaffold_stylesheet false
+    # g.template_engine :all
+    # g.fallbacks[:all] = :erb
+    # g.test_framework :rspec,
+    #                  controller_specs: false,
+    #                  view_specs: false,
+    #                  routing_specs: false,
+    #                  helper_specs: false
+
+    # g.test_framework :minitest, spec: false, fixture: true
+    # g.system_tests nil
+    # g.fallbacks[:minitest] = :rspec
+    # g.templates.unshift File::expand_path('../../../lib/templates', __FILE__)
+  end
+
+  # Apply UIkit alert form
+  # TODO append to gem
+  Rails.application.config.action_view.field_error_proc = proc do |html_tag, _instance_tag|
+    input_forms = %w[input textarea select]
+    html = html_tag
+    elem = Nokogiri::HTML::DocumentFragment.parse(html_tag).css 'label'
+    if elem.first
+      elem.first.attributes['class'].value += ' uk-text-danger'
+      html = elem.to_html.html_safe
+    else
+      input_forms.each do |form|
+        elem = Nokogiri::HTML::DocumentFragment.parse(html_tag).css(form)
+        if elem.first
+          elem.first.attributes['class'].value += ' uk-form-danger'
+          html = elem.to_html.html_safe
+        end
+      end
+    end
+    html
+  end
+CODE
+
+
 say <<-SAY
 ============================================================================
   template applied
